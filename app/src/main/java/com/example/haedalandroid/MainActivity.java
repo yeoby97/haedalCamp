f5288e7002a6b972ed24498708317a20bb07b0a4
이승호 <163648462+TTangho-Lee@users.noreply.github.com>
2025-03-01 16:27:24 +0900

    Update MainActivity.java

--- app/src/main/java/com/example/haedalandroid/MainActivity.java
+++ app/src/main/java/com/example/haedalandroid/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.haedalandroid;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -8,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
 
 import com.example.haedalandroid.user.currentUser;
 
@@ -19,7 +23,57 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView emailTextView = findViewById(R.id.emailTextView);
+        TextView passwordTextView=findViewById(R.id.passwordTextView);
+        Button homeButton=findViewById(R.id.btn_home);
+        Button searchButton=findViewById(R.id.btn_search);
+        Button galleryButton=findViewById(R.id.btn_gallery);
+        Button favoriteButton=findViewById(R.id.btn_favorite);
+        Button accountButton=findViewById(R.id.btn_account);
 
         emailTextView.setText(currentUser.email);
+
+        //인텐트를 받아서 저장된 정보를 가져온다.
+        Intent intent = getIntent();
+        String email=intent.getStringExtra("email");
+        String password=intent.getStringExtra("password");
+        emailTextView.setText(email);
+        passwordTextView.setText(password);
+
+        //버튼 클릭 시 프래그먼트를 띄우도록 클릭리스너를 설정한다
+        homeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new HomeFragment());
+            }
+        });
+        searchButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new SearchFragment());
+            }
+        });
+        galleryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new GalleryFragment());
+            }
+        });
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new FavoriteFragment());
+            }
+        });
+        accountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loadFragment(new AccountFragment());
+            }
+        });
+    }
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .commit();
     }
 }
